2e893527b123c7b0ed29b202507dd5302e9cedcb
dct <2382056614@qq.com>
2023-11-24 23:18:43 +0800

    vue3 项目初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-essential'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'parser': '@typescript-eslint/parser',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint',
+        'vue'
+    ],
+    // Eslint 代码规范，0代表忽略，1代表警告，2代表报错
+    'rules': {
+       // 在 vue 文件的 script 代码中只能使用单引号
+        'vue/singleline-html-element-content-newline': 'off',
+        'quotes': ['error', 'single', { 'avoidEscape': true }],
+        'vue/html-quotes': ['error', 'double'],
+        // 在 vue 文件的 script 代码中句尾不能加分号
+        'semi': ['error', 'never'],
+        // 在 vue 文件的 script 代码中等号左右要有空格
+        'space-infix-ops': 'error',
+        // 在 html 代码中不能使用单引号，只能用双引号
+    }
+}
---
+++ src/test/routeTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p v-text="test">
+    </p>
+    {{ a }}
+  </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const a = ref(1)
+let test = ref('wdwdw')
+</script>
+  
