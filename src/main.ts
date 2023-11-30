32fc07e04686efbbe9401983aa438ad4260a3731
Klein <11241686+Klein422@user.noreply.gitee.com>
2023-11-30 18:47:24 +0800

    [dev:导航栏]logo、模拟登录状态

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ const pinia = createPinia()
 import axios from 'axios'
 // import myAxios from './axios.ts'
 
-const app = createApp(App);
+const app = createApp(App)
 // 全局ctx(this) 上挂载 $axios
 app.config.globalProperties.$api = axios
-app.provide('axios', axios);
+app.provide('axios', axios)
 // app.provide('myAxios', myAxios);
 app.use(router)
 app.use(ElementPlus)
---
+++ src/view/layout/header/layoutHeader.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import userStatus from '@/view/layout/header/components/userStatus.vue'
+</script>
+<template>
+    <div class="header">
+        <RouterLink to="/">
+            <div class="logo">
+
+            </div>
+        </RouterLink>
+
+        <div class="user-status">
+            <userStatus></userStatus>
+        </div>
+    </div>
+</template>
+<style scoped>
+.header {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: flex;
+}
+
+.header .logo {
+    position: absolute;
+    height: 100%;
+    width: 100px;
+    background-image: url('/src/assets/logo/rvc.png');
+    background-repeat: no-repeat;
+    background-size: 150%;
+    float: left;
+    cursor: pointer;
+    background-position: center;
+}
+
+.header .user-status {
+    position: absolute;
+    height: 100%;
+    width: 100px;
+    right: 0;
+    margin-right: 10px;
+}
+</style>
