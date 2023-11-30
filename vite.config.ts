25b1983a007971775d00ef4f73a440dcaefe9dca
Klein <11241686+Klein422@user.noreply.gitee.com>
2023-11-30 20:02:43 +0800

    云端Mock代理更改

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
     proxy: {
       '/api':{
         target:'https://mock.apifox.com/',
-        changeOrigin:true,
-        rewrite: (path) => path.replace(/^\/api/,'')
       }
     }
   },
