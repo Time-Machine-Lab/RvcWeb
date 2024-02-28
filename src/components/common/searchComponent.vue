c26f13e8f49ff6b973eb0fae369c90ffd1e7a0ab
LisianthusLeaf <3106334435@qq.com>
2024-02-28 22:27:47 +0800

    导航栏修改

--- src/components/common/searchComponent.vue
+++ src/components/common/searchComponent.vue
@@ -1,3 +1,4 @@
+<!--顶部搜索栏组件-->
 <script lang="ts" setup>
 import { message } from '@/utils/message';
 import { ref } from 'vue';
@@ -12,38 +13,36 @@ const handleBlur = function () {
 </script>
 <template>
     <div class="search-box">
-        <div tabindex="-1" class="search-box__select" @click="searchSelectVisibility = !searchSelectVisibility"
-            @blur="handleBlur">
-            <span style="line-height: 35px;margin-left: 8px;margin-right: 15px;">{{ searchType[currentTypeIndex] }}</span>
-            <span>
-                <img width="14" height="14" class="vertical-center" src="/icon/arrow-down.svg">
-            </span>
+        <!--切换搜索按钮-->
+        <div tabindex="-1" class="search-box__select" @click="searchSelectVisibility = !searchSelectVisibility" @blur="handleBlur">
+          <span >{{ searchType[currentTypeIndex] }}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="white" class="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+          </svg>
         </div>
+        <!--搜索项选择下拉框-->
         <div class="search-select" v-show="searchSelectVisibility">
-            <div class="search-select__item" v-for="(method, index) in searchType"
+            <div class="search-select__item flex" v-for="(method, index) in searchType"
                 :style="{ backgroundColor: currentTypeIndex == index ? 'rgba(24,100,171)' : '' }" :key="index"
                 @click="currentTypeIndex = index; searchSelectVisibility = false">{{ method }}
             </div>
         </div>
+        <!--搜索内容输入框-->
         <div class="search-box__input" >
-            <input :placeholder="'搜索'+searchType[currentTypeIndex]"  disabled class="input">
+            <input :placeholder="'搜索'+searchType[currentTypeIndex]">
         </div>
+        <!--搜索按钮-->
         <div class="search-box__button">
             <img class="vh-center" @click="message.warning('开发中')" src="/icon/search.svg">
         </div>
-
     </div>
 </template>
 <style scoped>
 .search-box {
     position: relative;
-    height: 54%;
-    width: 60%;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
+    height: 40px;
+    width: 100%;
     border-radius: 5px;
-    background-color: rgba(52, 58, 64);
     display: flex;
     user-select: none;
     font-family: 'ZCool';
@@ -51,69 +50,67 @@ const handleBlur = function () {
 
 .search-box__select {
     position: relative;
-    width: calc(15% - 2px);
-    min-width: 70px;
-    height: calc(100% - 2px);
+    width: 20%;
+    height: 100%;
     border: rgba(52, 58, 64) 1px solid;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     cursor: pointer;
     display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
     outline: none;
+  background-color: rgb(58, 58, 58);
 }
 
-.search-box__select:focus {
+.search-box__select:focus{
     outline: none;
     border: rgba(25, 113, 194) 1px solid;
 }
 
 .search-box__select span {
-    position: relative;
-    display: inline-block;
-    font-size: 14px;
-    color: rgba(255, 255, 255, 0.7);
+  margin-right: 10px;
+  font-size: 14px;
+  color: rgba(255, 255, 255, 0.7);
 }
 
 .search-box__input {
-    width: 80%;
-    height: 100%;
-}
-
-.input {
+  position: relative;
+  width: 80%;
+  height: 100%;
+  background-color: rgba(37, 38, 43);
+  border: rgb(58, 58, 58) 2px solid;
+  input {
     position: relative;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    border: rgba(52, 58, 64) 1px solid;
-
-    background-color: rgba(37, 38, 43);
-    width: calc(100% - 32px);
-    height: calc(100% - 2px);
-    padding: 0 15px;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
     font-size: 16px;
     color: white;
     outline: none;
-    border: none;
-}
-input:hover{
-    cursor:not-allowed
-}
-.input::placeholder{
+  }
+  input::placeholder{
     font-family: 'ZCool';
-}
+    padding-left: 10px;
+  }
 
-.input:focus {
+  input:focus {
     outline: none;
     border: rgba(25, 113, 194) 1px solid;
-}
+    padding-left: 10px;
+  }
 
+}
 .search-box__button {
     position: relative;
-    width: 5%;
+    width: 10%;
     min-width: 35px;
     border-top-right-radius: 5px;
     border-bottom-right-radius: 5px;
     height: 100%;
     cursor: pointer;
+  background-color: rgb(58, 58, 58);
 }
 
 .search-box__button:hover {
@@ -121,22 +118,24 @@ input:hover{
 }
 
 .search-select {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
     position: absolute;
-    margin-top: 60px;
-    width: 100px;
-    border-radius: 10px;
+    margin-top: 50px;
+    width: 20%;
+    border-radius: 5px;
     border: rgba(55, 58, 64) 1px solid;
     background-color: rgba(37, 38, 43);
-    padding: 5px;
     z-index: 10;
     user-select: none;
 }
 
 .search-select__item {
-    padding-left: 15px;
-    width: calc(100% - 0px);
-    height: 40px;
-    line-height: 40px;
+  margin-top:5px;
+  margin-bottom: 5px;
+    width: 90%;
+    height: 35px;
     font-size: 14px;
     text-align: left;
     border-radius: 5px;
--- src/components/user/userStatus.vue
+++ src/components/user/userStatus.vue
@@ -69,11 +69,11 @@ onMounted(() => {
 })
 </script>
 <template>
-    <div class="user-status">
+    <div>
 <!--登陆弹窗-->
       <LoginComponent v-if="LoginOpen" class="login" @close="LoginClose" @login="changeLoginState"></LoginComponent>
 <!--未登录状态-->
-      <div class="login-button" v-if="!userProfile?.uid" @click="LoginOpen=true">
+      <div class="login-button flex" v-if="!userProfile?.uid" @click="LoginOpen=true">
         登录
       </div>
 <!--登陆状态-->
@@ -107,97 +107,4 @@ onMounted(() => {
 </template>
 <style scoped>
 @import "@/components/user/style/userStatus.css";
-.login{
-  position: fixed;
-  width:100%;
-  height:100%;
-  top:0;
-  left:0;
-}
-.user-status {
-    position:absolute;
-    height: 100%;
-    width: 100%;
-    border-left: rgba(255, 255, 255, 0.1) 1px solid;
-}
-
-.user-status .login-button {
-    position: relative;
-    height: 30px;
-    width: 70px;
-    background-color: transparent;
-    border: rgba(237, 130, 201) 1px solid;
-    left: 50%;
-    top: 50%;
-    color: rgba(237, 130, 201);
-    transform: translate(-50%, -50%);
-    line-height: 30px;
-    font-weight: 700;
-    border-radius: 8px;
-    user-select: none;
-    cursor: pointer;
-    transition: all 0.3s;
-}
-
-.user-status .login-button:hover {
-    border: rgba(217, 110, 181) 1px solid;
-    color: rgba(217, 110, 181);
-}
-
-.user-status .avatar-container {
-    position: relative;
-    height: 40px;
-    width: 40px;
-    left: 50%;
-    top: 50%;
-    cursor: pointer;
-    border-radius: 20px;
-    /* overflow: hidden; */
-    transform: translate(-50%, -50%);
-}
-
-.user-status .avatar {
-    position: relative;
-    height: 40px;
-    width: 40px;
-    cursor: pointer;
-    border-radius: 20px;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    transition: all 0.5s;
-}
-
-.user-more {
-    position: absolute;
-    top: 65px;
-    right: 0px;
-    width: 200px;
-    border-radius: 5px;
-    border: rgba(55, 58, 64) 1px solid;
-    background-color: rgba(37, 38, 43);
-    padding: 5px;
-    z-index: 10;
-    user-select: none;
-}
-
-.user-more__item {
-    padding-left: 15px;
-    width: calc(100% - 0px);
-    height: 50px;
-    font-size: 14px;
-    border-radius: 5px;
-    text-align: left;
-    color: rgba(255, 255, 255, 0.7);
-    font-family: 'ZCool';
-}
-
-.user-more__item:hover {
-    background-color: rgba(56, 58, 64);
-    cursor: pointer;
-}
-
-/* .user-status .avatar:hover {
-    scale: 1.1;
-} */
 </style>
